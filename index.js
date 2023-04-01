eaa50510913df757cb271dad938491f0051c0c8b
fatmamontaser <89998228+fatmamontaser@users.noreply.github.com>
2023-04-01 22:59:57 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let imgOne = document.querySelector(".imgOne")
+let container =document.querySelector(".container")
+
+
+function phones(phone){
+imgOne.src = phone;
+
+}
+
+function colors(colors){
+
+container.style.background = colors
+
+}
